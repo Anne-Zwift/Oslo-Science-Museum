06caf17909e58557e4c33768e4fc5ba9100c31f4
Anne-Zwift <145778485+Anne-Zwift@users.noreply.github.com>
2025-05-20 13:37:33 +0200

    adding JavaScript, CSS, html and adding new slider

---
+++ javaScript/index.js
@@ -0,0 +1,31 @@
+new Swiper('.card-wrapper', {
+  loop: true,
+  spaceBetween: 30,
+
+  // pagination bullets
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  //responsive breakpoints
+  breakpoints: {
+    0: {
+      slidesPerView: 1
+    },
+    768: {
+    slidesPerView: 2
+    },
+     1024: {
+    slidesPerView: 3
+    },
+  }
+
+});
